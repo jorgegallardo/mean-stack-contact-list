f6f4cc8de464645cb9d39a2274c937c32e0a0291
unknown <jorge8486@gmail.com>
2015-09-02 01:02:59 -0400

    finished edit and update contacts

--- public/controllers/AppController.js
+++ public/controllers/AppController.js
@@ -27,4 +27,22 @@ app.controller('AppController', ['$scope', '$http', function ($scope, $http) {
       refresh();
     });
   };
+
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/contactlist/' + id).success(function (response) {
+      $scope.contact = response;
+    });
+  };
+
+  $scope.update = function(id) {
+    console.log($scope.contact._id);
+    $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function (response){
+      refresh();
+    });
+  };
+
+  $scope.deselect = function() {
+    $scope.contact = '';
+  };
 }]);

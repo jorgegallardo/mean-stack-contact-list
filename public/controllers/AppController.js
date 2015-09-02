d7632dac079497a663b1f43fadcfb44a69293f50
unknown <jorge8486@gmail.com>
2015-09-02 00:17:00 -0400

    created mongodb, add contact to db

--- public/controllers/AppController.js
+++ public/controllers/AppController.js
@@ -3,9 +3,22 @@ var app = angular.module('myApp', []);
 app.controller('AppController', ['$scope', '$http', function ($scope, $http) {
   console.log('Hello World from controller');
 
-  $http.get('/contactList').success(function (response) {
-    console.log('I got the data I requested');
-    $scope.contactList = response;
-  })
+  var refresh = function() {
+    $http.get('/contactlist').success(function (response) {
+      console.log('I got the data I requested');
+      $scope.contactlist = response;
+      $scope.contact = '';
+    });
+  };
+
+  refresh();
+
+  $scope.addContact = function() {
+    console.log($scope.contact);
+    $http.post('/contactlist', $scope.contact).success(function (response) { // $scope.contact is the data sent to the server
+      console.log(response);
+      refresh();
+    }); 
+  };
 }]);
 
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('/contactList', function (req, res) {
+app.get('/contactlist', function (req, res) {
   console.log("I received a GET request");
-  var person1 = {
-    name: 'Tim',
-    email: 'tim@email.com',
-    number: '(111) 111-1111'
-  };
-  var person2 = {
-    name: 'Emily',
-    email: 'emily@email.com',
-    number: '(222) 222-2222'
-  };
-  var contactList = [person1, person2];
-  res.json(contactList);
+  db.contactlist.find(function (error, docs) {
+    console.log(docs);
+    res.json(docs); //sends data back to controller
+  });
+  // var person1 = {
+  //   name: 'Tim',
+  //   email: 'tim@email.com',
+  //   number: '(111) 111-1111'
+  // };
+  // var person2 = {
+  //   name: 'Emily',
+  //   email: 'emily@email.com',
+  //   number: '(222) 222-2222'
+  // };
+  // var contactlist = [person1, person2];
+  // res.json(contactlist);
+});
+
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function (err, doc) {
+    res.json(doc);
+  });
 });
 
 app.listen(3000);
